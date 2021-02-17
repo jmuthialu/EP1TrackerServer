fabdbe952b806958138105490d8465ef2ac26eab
jmuthialu <jmuthialu@yahoo.com>
2021-02-17 00:53:20 -0500

    Seperated data into separate module

--- server/server.js
+++ server/server.js
@@ -1,38 +1,5 @@
 const express = require('express');
-
-const epallets = 
-  [
-    {
-      id: 100, 
-      model: 'EP1-100x', 
-      status: 'Moving', 
-      imageName: 'pallet',
-      coordinate: {
-        latitude: 33.753746,
-        longitude: -84.386330
-      }
-    },
-    {
-      id: 101, 
-      model: 'EP1-100x', 
-      status: 'Moving', 
-      imageName: 'pallet',
-      coordinate: {
-        latitude: 33.743746,
-        longitude: -84.396330
-      }
-    },
-    {
-      id: 102, 
-      model: 'EP1-100x', 
-      status: 'Stopped', 
-      imageName: 'pallet',
-      coordinate: {
-        latitude: 33.763746,
-        longitude: -84.376330
-      }
-    },
-  ];
+const { epallets } = require('../app_data/data.js')
 
 const app = express();
 app.use(express.static('public'));
